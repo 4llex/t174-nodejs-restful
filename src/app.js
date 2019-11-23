77c650f2258913404528c00ecfb17d010bebc737
Alex Rosa <alexr@gec.inatel.br>
2019-11-23 14:36:43 -0300

    added extrenal middlewares

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Middlewares
+const notFound= require('./middleware/notfound')
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev')); // mostra os requests do usuario no terminal 
+
+// Routes
 const taskRouter = require('./routes/tasks');
 app.use('/api/tasks', taskRouter);
 
-const notFound= require('./middleware/notfound')
+// Missing Routes
 app.use(notFound);
 
 module.exports = app;
